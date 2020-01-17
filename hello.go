6a921c2fedb084b07f4aafb0406bdaf715d8b909
wrekone <wrekone@users.noreply.github.com>
2020-01-16 17:48:26 -0800

    add Spanish conditional

--- hello.go
+++ hello.go
@@ -8,6 +8,10 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
 	return englishHelloPrefix + name
 }
 
