0707cd078f0c0f042eed87fe45e3e05b771baac4
wrekone <wrekone@users.noreply.github.com>
2020-01-16 17:52:55 -0800

    replace strings with variables

--- hello.go
+++ hello.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
